bc762c2efc5bf903cac1ce6299e766500f73d50b
JanC238 <237881148@qq.com>
2016-08-01 11:37:14 +0800

    注释

--- frontend/controllers/ArticleController.php
+++ frontend/controllers/ArticleController.php
@@ -14,12 +14,22 @@ use yii\web\Controller;
 
 class ArticleController extends Controller
 {
+    /**
+     * 文章列表
+     * @param $cate_id
+     * @return string
+     */
     public function actionIndex($cate_id)
     {
         $articles = Article::findAll(['cate_id' => $cate_id]);
         return $this->render('index', ['articles' => $articles]);
     }
 
+    /**
+     * 文章详情
+     * @param $id
+     * @return string
+     */
     public function actionView($id)
     {
         $article = Article::findOne($id);
